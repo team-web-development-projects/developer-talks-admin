53527bf3fd7618526738b89cf45b19fdbcd8d74f
ssing_7942 <djagmlwnn12@gmail.com>
2023-09-12 21:11:53 +0900

    그래프 수정완료

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Bar, BarChart, XAxis, YAxis } from "recharts";
@@ -10,9 +10,19 @@ const Main = () => {
   const [chartData, setChartData] = useState([]);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Use useEffect to fetch data when startDate and endDate change
+  // useEffect(() => {
+  //   if (startDate && endDate) {
+  //     getUserList();
+  //   }
+  // }, [startDate, endDate]);
 
   async function getUserList() {
     try {
+      setIsLoading(true);
+
       const { data } = await axios.get(`${ROOT_API}/admin/visitors/daily-count`, {
         params: { startDate: startDate.toISOString().slice(0, 10), endDate: endDate.toISOString().slice(0, 10) },
         headers: {
@@ -26,17 +36,17 @@ const Main = () => {
         name: date,
         num: data[date],
       }));
+
+      // Sort chartData by date
+      chartData.sort((a, b) => new Date(a.name) - new Date(b.name));
+
       setChartData(chartData);
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
     }
   }
-
-  useEffect(() => {
-    if (startDate && endDate) {
-      getUserList();
-    }
-  }, [startDate, endDate]);
 
   const handleStartDateChange = (date) => {
     setStartDate(date);
@@ -47,26 +57,37 @@ const Main = () => {
   };
 
   return (
-    <>
-      <BarChart width={500} height={200} data={chartData}>
-        <Bar dataKey="num" fill="#8884d8" />
-        <XAxis dataKey="name" />
-        <YAxis />
-      </BarChart>
-      <div>
-        <h1>시작 날짜 선택</h1>
-        <DatePicker selected={startDate} onChange={handleStartDateChange} />
-        {startDate && <p>시작 날짜: {startDate.toDateString()}</p>}
+    <div className="container">
+      <div className="bg-white shadow-lg rounded-lg p-4 mb-4">
+        {chartData.length > 0 && (
+          <BarChart width={800} height={400} data={chartData}>
+            <Bar dataKey="num" fill="#8884d8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+          </BarChart>
+        )}
       </div>
-      <div>
-        <h1>끝 날짜 선택</h1>
-        <DatePicker selected={endDate} onChange={handleEndDateChange} />
-        {endDate && <p>끝 날짜: {endDate.toDateString()}</p>}
+
+      <div className="bg-white shadow-lg rounded-lg p-4">
+        <h1 className="text-xl font-bold mb-4">날짜 선택</h1>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">시작 날짜</label>
+            <DatePicker className="border rounded p-2 w-full" selected={startDate} onChange={handleStartDateChange} />
+            {startDate && <p className="mt-2 text-sm text-gray-500">시작 날짜: {startDate.toDateString()}</p>}
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">끝 날짜</label>
+            <DatePicker className="border rounded p-2 w-full" selected={endDate} onChange={handleEndDateChange} />
+            {endDate && <p className="mt-2 text-sm text-gray-500">끝 날짜: {endDate.toDateString()}</p>}
+          </div>
+        </div>
+
+        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={getUserList} disabled={!startDate || !endDate || isLoading}>
+          날짜 선택
+        </button>
       </div>
-      <button onClick={getUserList} disabled={!startDate || !endDate}>
-        날짜 선택
-      </button>
-    </>
+    </div>
   );
 };
 
